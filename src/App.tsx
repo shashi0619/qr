2cbed4e12282cc80a2e0c0077d0e106991c8fd6a
shashi s <shashichintu68@gmail.com>
2025-03-06 12:57:15 +0530

    qrcode

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { DarkThemeToggle } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+  
+
+<div className="flex items-center justify-center min-h-screen rounded-lg">
+  <div className="max-w-sm bg-white border-gray-200 rounded-lg shadow ">
+    <div className="p-14 bg-slate-400 rounded-lg ">
+      <img className="rounded-lg " src="./public/qqq.png" alt=""></img>
+    </div>
+    
+    <div className="p-6">
+      <h5 className="mb-3 text-center text-2xl font-bold text-gray-900">
+        Improve your front-end skills by building projects
+      </h5>
+      <p className="mb-3  text-center text-gray-700 ">
+        Scan the QR code to visit Frontend Mentor and take your code to the next level
+      </p>
+    </div>
+  </div>
+  
+</div>
+
+
+    </>
+  );
+}
+
+export default App;
